f36be7c89301a79eda3286b45af77469f70b143d
JWin23 <iamkaungthantwin@gmail.com>
2023-11-09 23:14:07 -0600

    functions working. css need some more touches

--- react-app/src/Routes.jsx
+++ react-app/src/Routes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes as DomRoutes } from "react-router-dom";
 import Home from "./pages/Home";
 import Solve from "./pages/Solve";
 import Result from "./pages/Result";
+import Leaderboard from "./pages/Leaderboard"
 
 export default function Routes() {
     return (
@@ -9,6 +10,7 @@ export default function Routes() {
             <Route path='/' element={<Home/>} />
             <Route path='/solve/:problem' element={<Solve/>}></Route>
             <Route path='/result' element={<Result/>}></Route>
+            <Route path='/leaderboard' element={<Leaderboard/>}></Route>
         </DomRoutes>
     );
 }
