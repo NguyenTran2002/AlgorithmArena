028d9c372f4de961c9a18f85a8f819cfb76b7658
NguyenTran2002 <nguyen.tran.2002.work@gmail.com>
2023-11-18 14:01:40 -0600

    added Loading Problem page to Solve.jsx

--- react-app/src/pages/Solve.jsx
+++ react-app/src/pages/Solve.jsx
@@ -7,99 +7,119 @@ import MarkdownIt from 'markdown-it';
 import Split from 'react-split';
 import { Button } from '@mui/material';
 import axios from 'axios';
+import { HashLoader } from 'react-spinners'
 
 function Solve() {
-  
-    // Extract the 'problem' parameter from the URL
-    const { problem } = useParams();
-    const [markdownContent, setMarkdownContent] = useState('');
-    const navigate = useNavigate()
-    const [problemName, setProblemName] = useState('');
-    const database_url = 'http://127.0.0.1:7432'
 
-    const [value, setValue] = useState();
-    const onChange = useCallback((val) => {
+  // Extract the 'problem' parameter from the URL
+  const { problem } = useParams();
+  const [markdownContent, setMarkdownContent] = useState('');
+  const navigate = useNavigate()
+  const [problemName, setProblemName] = useState('');
+  const database_url = 'http://127.0.0.1:7432'
+
+  const [value, setValue] = useState();
+  const onChange = useCallback((val) => {
     //   console.log('val:', val);
-      setValue(val);
-    }, []);
+    setValue(val);
+  }, []);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        async function fetchResult() {
-          try {
-            const post_data = {
-              'problem': problem,
-    
-            };
-            // post request with data
-            const response = await axios.post(database_url + '/get_problem_md_and_arguments', post_data); // Replace with your API endpoint
-            //console.log(response)
-            const markdown = response.data['markdown'];
-            const args = response.data['arguments'];
-            const joinedArgs = args.join(', ');
+    async function fetchResult() {
+      try {
+        const post_data = {
+          'problem': problem,
 
-            setProblemName(`class Solution(object):\n  def ${problem}(self, ${joinedArgs}):\n  # Write your solution here\n\n`)
+        };
+        // post request with data
+        const response = await axios.post(database_url + '/get_problem_md_and_arguments', post_data); // Replace with your API endpoint
+        //console.log(response)
+        const markdown = response.data['markdown'];
+        const args = response.data['arguments'];
+        const joinedArgs = args.join(', ');
 
-            const md = new MarkdownIt();
-            const htmlContent = md.render(markdown);
-    
-            setMarkdownContent(htmlContent); 
-    
-          } catch (error) {
-            console.error(`Error while retrieving problems: ${error.message}`);
-          }
-        }
-    
-        fetchResult();
-      }, []);
-    
+        // setMarkdownContent(markdown);
 
-    const redirectPage = () => {
-        navigate('/result', {state:{problem:problem, user_code:value}});
+        setProblemName(`class Solution:\n  def ${problem}(self, ${joinedArgs}):\n  # Write your solution here\n\n`)
+
+        const md = new MarkdownIt();
+        const htmlContent = md.render(markdown);
+
+        setMarkdownContent(htmlContent);
+
+      } catch (error) {
+        console.error(`Error while retrieving problems: ${error.message}`);
+      }
     }
 
-    return (
-        <div>
-            <h1 className='h1'>The Clock has Started!</h1>
-            <Split
-                sizes={[50, 50]} // Initial sizes of the panes in percentages
-                minSize={100} // Minimum size for a pane
-                expandToMin={false} // Whether to expand to the minimum size when resizing
-                gutterSize={50} // Size of the dividers (gutters)
-                direction='horizontal'
-                mode='horizontal'
-                className='solve-container'
-            >
-                <div 
-                    dangerouslySetInnerHTML={{ __html: markdownContent }}
-                    style={{
-                        /* Define your inline styles here */
-                        fontSize: '16px',
-                        lineHeight: '1.5',
-                        textAlign: 'left',
-                        width: '50vh',
-                        /* Add more styles as needed */
-                    }}
-                    className="markdown-content"
-                >
-                </div>
-                <div className='w-full overflow-auto'>
-                    <CodeMirror
-                        value={problemName}
-                        theme={vscodeDark}
-                        // onChange={onChange}
-                        extensions={[python()]}
-                        style={{textAlign: 'left'}}
-                        className="code-mirror-container"
-                        onChange={onChange}
-                    />
-                    <Button onClick={redirectPage}>Submit</Button>
-                </div>
-            </Split>
-        </div>
-    );
+    fetchResult();
+  }, []);
+
+
+  const redirectPage = () => {
+    navigate('/result', { state: { problem: problem, user_code: value } });
   }
 
-  
-  
-  export default Solve;
+  return (
+    <div>
+      {
+        markdownContent == '' ?
+          <div>
+            <h1>Loading Problem</h1>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: '80px',
+            }}>
+              <HashLoader color="#36d7b7" size='100' />
+            </div>
+          </div>
+          :
+          <div>
+            <h1 className='h1'>The Clock has Started!</h1>
+            <Split
+              sizes={[50, 50]} // Initial sizes of the panes in percentages
+              minSize={100} // Minimum size for a pane
+              expandToMin={false} // Whether to expand to the minimum size when resizing
+              gutterSize={50} // Size of the dividers (gutters)
+              direction='horizontal'
+              mode='horizontal'
+              className='solve-container'
+            >
+              <div
+                dangerouslySetInnerHTML={{ __html: markdownContent }}
+                style={{
+                  /* Define your inline styles here */
+                  fontSize: '16px',
+                  lineHeight: '1.5',
+                  textAlign: 'left',
+                  width: '50vh',
+                  /* Add more styles as needed */
+                }}
+                className="markdown-content"
+              >
+              </div>
+              <div className='w-full overflow-auto'>
+                <CodeMirror
+                  value={problemName}
+                  theme={vscodeDark}
+                  // onChange={onChange}
+                  extensions={[python()]}
+                  style={{ textAlign: 'left' }}
+                  className="code-mirror-container"
+                  onChange={onChange}
+                />
+                <Button onClick={redirectPage}>Submit</Button>
+              </div>
+            </Split>
+          </div>
+      }
+    </div>
+  );
+}
+
+
+
+export default Solve;
